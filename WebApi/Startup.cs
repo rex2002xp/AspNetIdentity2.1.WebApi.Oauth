01fb465683c96e5834ea7398f499849d8f128f51
Victor Cornejo <rex2002xp@gmail.com>
2015-04-07 00:05:09 -0600

    Paso 6. Aunque ya tenemos todo configurado para utilizar JWT y hemos protegido el webservice todavía hace falta agregar la capacidad para analizar el token que vendrá adjunto en todas las peticiones, de esta forma autorizar la petición.

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 using Owin;
@@ -21,6 +24,7 @@ namespace WebApi
         {
             HttpConfiguration httpConfig = new HttpConfiguration();
             ConfigureOAuthTokenGeneration(app);
+            ConfigureOAuthTokenConsumption(app);
             ConfigureWebApi(httpConfig);
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(httpConfig);
@@ -33,7 +37,7 @@ namespace WebApi
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
-            string audienceId = ConfigurationManager.AppSettings["as:IssueServer"];
+            string issueServer = ConfigurationManager.AppSettings["as:IssueServer"];
 
             // La configuracion del Plugin para OAuth bearer se debe realizar aca.
             OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
@@ -43,12 +47,32 @@ namespace WebApi
                 TokenEndpointPath = new PathString("/oauth/token"),
                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
                 Provider = new CustomOAuthProvider(),
-                AccessTokenFormat = new CustomJwtFormat(audienceId)
+                AccessTokenFormat = new CustomJwtFormat(issueServer)
             };
 
             // OAuth 2.0 Bearer Access Token Generation
             app.UseOAuthAuthorizationServer(OAuthServerOptions);
 
+        }
+
+        private void ConfigureOAuthTokenConsumption(IAppBuilder app)
+        {
+
+            string issueServer = ConfigurationManager.AppSettings["as:IssueServer"];
+            string audienceId = ConfigurationManager.AppSettings["as:AudienceId"];
+            byte[] audienceSecret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["as:AudienceSecret"]);
+
+            // Los controladores tipo API que esten marcados con [Authorize] podran ser validados con el Token JWT.
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audienceId },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issueServer, audienceSecret)
+                    }
+                });
         }
 
         private void ConfigureWebApi(HttpConfiguration config)
